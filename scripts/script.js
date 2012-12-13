113ece231c8cd3aa5933961c1f8590f11e002a67
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-12-13 10:40:23 +0530

    new design

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
-	var todoView = ToDoView.getInstance();
-});
+	var Events = (function() {
+		var addNewItem = function() {
+			var todo_list = $('.todo-list'),	
+				newItem = $('<li><div class="sidebar"><span class="uncheck"></span></div></li>');
 
-$(window).unload(function(e){
-	localStorage.setItem('ToDo',JSON.stringify(ToDoModel.getCategories()));
+			newItem.append('<div class="task"><span class="editable" contentEditable="true"></span></div>')	
+			todo_list.append(newItem);
+		};
+
+		return {
+			bindEvents : function() {
+				$('.list li').hover(function() {
+					$(this).addClass('selected');
+					$(this).siblings().removeClass();
+				});
+
+				$('.sidebar').on('click', '.uncheck', function() {
+					$(this).removeClass();
+					$(this).addClass('check');
+				});
+
+				$('.sidebar').on('click', '.check', function(){
+					$(this).removeClass();
+					$(this).addClass('uncheck');
+				});
+
+				$('.add-new').on('click', addNewItem);
+			}
+		}
+	})();
+	
+
+	Events.bindEvents();
 });
