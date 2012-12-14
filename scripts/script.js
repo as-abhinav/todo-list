8966f43787b84c6ee5ab418f13193298788d14d1
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-12-14 21:52:28 +0530

    New category can be added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	var Events = (function() {
 
-		var addNewItem = function() {
+		var category_list = $('#category-list'),
+
+
+		addNewItem = function() {
 			var todo_list = $('.todo-list'),	
 				newItem = $('<li><div class="sidebar"><span class="uncheck"></span></div></li>'),
 
@@ -11,7 +14,7 @@ $(document).ready(function(){
 					itemId = todo_list.children().length + 1;
 
 				inputBox.keyup(function(event) {
-					if (event.keyCode === 13){
+					if (event.keyCode === 13) {
 						var text = inputBox.val();
 						taskDiv.empty();
 						taskDiv.append('<span>' + text + '</span>');
@@ -23,11 +26,18 @@ $(document).ready(function(){
 			} 
 			newItem.append(prepareInputBox());	
 			todo_list.append(newItem);
+		},
+		addNewCategory = function(event) {
+			if(event.keyCode === 13) {
+				var newCategory = $('<li>' + $(this).val() + '</li>');
+				category_list.append(newCategory);
+				$(this).val('');
+			}
 		};
 
 		return {
 			bindEvents : function() {
-				$('.list li').hover(function() {
+				category_list.on('hover', 'li', function() {
 					$(this).addClass('selected').siblings().removeClass();
 				});
 
@@ -35,9 +45,21 @@ $(document).ready(function(){
 					$(this).removeClass().addClass('check');
 				});
 
-				$('.todo-list').on('click', ' .sidebar .check', function(){
+				$('.todo-list').on('click', '.sidebar .check', function(){
 					$(this).removeClass().addClass('uncheck');
 				});
+
+				$('.list .add-category input').focus(function() {
+					$(this).addClass('adding');
+					$(this).attr('placeholder', '');
+				});
+
+				$('.list .add-category input').blur(function(){ 
+					$(this).removeClass('adding');
+					$(this).attr('placeholder', 'Create New List...');
+				});
+
+				$('.list .add-category input').keyup(addNewCategory);
 
 				$('.add-new').on('click', addNewItem);
 			}
